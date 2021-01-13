08b378713e094a31243ae3e6321b83f7e176a3d0
0617yoneda <0617yoneda@gmail.com>
2021-01-13 06:20:56 +0000

    [add]本番用 完成アニメーション

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,5 @@
 document.addEventListener('turbolinks:load', function() {
     $('.loading').delay(500).fadeOut(300);
 });
-function stopload(){
-    $('.loading').delay(1000).fadeOut(700);
-}
+
 
